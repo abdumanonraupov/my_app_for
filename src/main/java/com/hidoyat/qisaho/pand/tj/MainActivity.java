5a69cf1d7b923455889bc612c5c2b0b9e0d7f415
abdumanonraupov <73402817+abdumanonraupov@users.noreply.github.com>
2020-11-29 22:36:07 +0300

    Initial commit

---
+++ src/main/java/com/hidoyat/qisaho/pand/tj/MainActivity.java
@@ -0,0 +1,24 @@
+package com.hidoyat.qisaho.pand.tj;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupMenu();
+    }
+     private void setupMenu() {
+        FragmentManager fm = getSupportFragmentManager();
+        MenuFragmentList mMenuFragment = (MenuFragmentList) fm.findFragmentById(R.id.id_container_menu);
+        if (mMenuFragment == null) {
+            mMenuFragment = new MenuFragmentList();
+            fm.beginTransaction().add(R.id.id_container_menu, mMenuFragment).commit();
+        }
+    }
+}
